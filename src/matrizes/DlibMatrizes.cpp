186b61079e7724d2c4c0fca164ce3b261d297e7a
NtanBraga <natanybraga@gmail.com>
2026-09-09 21:16:27 -0300

    new folder for matrixes

--- src/matrizes/DlibMatrizes.cpp
+++ src/matrizes/DlibMatrizes.cpp
@@ -79,12 +79,5 @@ int main(){
 
     cout << "incrementar parte da matrix: \n" << result4 << endl;
 
-    int num1 = 1;
-    int num2 = 2;
-
-    int num3 = num1 + num2;
-
-    cout << num3;
-
     return 0;
 }
